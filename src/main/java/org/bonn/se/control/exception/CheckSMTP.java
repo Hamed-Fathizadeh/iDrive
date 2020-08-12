f1d91fbb766c5094c08c363b13278c8f032557b1
shasso2s <hassouni.salima@outlook.com>
2020-08-13 00:48:46 +0200

    configution of smtp

--- src/main/java/org/bonn/se/control/exception/CheckSMTP.java
+++ src/main/java/org/bonn/se/control/exception/CheckSMTP.java
@@ -1,4 +1,16 @@
 package org.bonn.se.control.exception;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class CheckSMTP {
+    public static boolean pingHost(String host, int port, int timeout) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), timeout);
+            return true;
+        } catch (IOException e) {
+            return false; // Either timeout or unreachable or failed DNS lookup.
+        }
+    }
 }
