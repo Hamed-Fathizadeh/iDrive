efd180304e3fa2b513d1ec31e6a4cfd25e109458
shasso2s <hassouni.salima@outlook.com>
2020-08-13 01:37:31 +0200

    login control

---
+++ src/main/java/org/bonn/se/control/exception/LoginControl.java
@@ -0,0 +1,105 @@
+package org.bonn.se.control.exception;
+
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.UI;
+import org.bonn.se.gui.ui.MyUI;
+import org.bonn.se.model.objects.entitites.Endkunde;
+import org.bonn.se.model.objects.entitites.User;
+import org.bonn.se.model.objects.entitites.Vertriebler;
+import org.bonn.se.services.util.db.exception.DatabaseException;
+import org.bonn.se.services.util.db.exception.JDBCConnection;
+import org.bonn.se.services.util.Views;
+import org.bonn.se.services.util.Roles;
+import org.bonn.se.model.dao.ProfilDAO;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginControl {
+    private LoginControl(){
+
+    }
+
+    private static LoginControl instance;
+
+    public static LoginControl getInstance() {
+        if (instance == null){
+            instance = new LoginControl();
+        }
+        return instance;
+    }
+    public void checkAuthentication ( String login , String password) throws NoSuchUserOrPassword, DatabaseException, SQLException {
+
+        ResultSet set;
+        Statement statement = null;
+
+        String c = "'";
+        if(password.indexOf('/') != -1 || password.indexOf(c.charAt(0)) != -1){
+            throw new DatabaseException("Die Zeichen ' oder / sind nicht als Passwort erlaubt!.");
+        }
+
+        try {
+            statement = JDBCConnection.getInstance().getStatement();
+            set = statement.executeQuery("SELECT * "
+                    + "FROM iDrive.tab_user "
+                    + "WHERE upper(iDrive.tab_user.email) = '" +login.toUpperCase() + "'"
+                    + " AND iDrive.tab_user.passwort = '" + password + "'");
+
+        } catch (SQLException | DatabaseException throwables) {
+            Logger.getLogger(LoginControl.class.getName()).log(Level.SEVERE,null,throwables);
+            throw new DatabaseException("Fehler im SQL Befehl! Bitte den Programmierer benachrichtigen.");
+        }
+        User user;
+        try {
+            if ( set.next()) {
+
+                user = new User();
+                user.setVorname(set.getString("vorname"));
+                user.setNachname(set.getString("nachname"));
+                user.setEmail((set.getString(1)));
+                user.setPasswort(set.getString(2));
+                user.setType(set.getString(5));
+
+                if(set.getString(5).equals("V")) {
+                    Vertriebler vertriebler = new Vertriebler();
+                    MyUI.getCurrent().getSession().setAttribute(Roles.Vertriebler,vertriebler);
+                    vertriebler.setEmail(user.getEmail());
+                    vertriebler.setVorname(user.getVorname());
+                    vertriebler.setNachname(user.getNachname());
+                    //vertriebler = ProfilDAO.getInstance().getUnternehmenProfil(vertriebler);
+
+                    UI.getCurrent().getSession().setAttribute(Roles.Vertriebler,vertriebler);
+
+                } else if(set.getString(5).equals("E")) {
+                    Endkunde endkunde;
+                   //endkunde = ProfilDAO.getInstance().getStudent(user.getEmail());
+
+                   // UI.getCurrent().getSession().setAttribute(Roles.Endkunde, endkunde);
+                }
+            }else{
+                throw new NoSuchUserOrPassword();
+            }
+        } catch (SQLException throwables) {
+            Logger.getLogger(JDBCConnection.class.getName()).log(Level.SEVERE, null, throwables);
+        } finally {
+            set.close();
+            JDBCConnection.getInstance().closeConnection();
+        }
+
+        UI.getCurrent().getNavigator().navigateTo(Views.LOGINVIEW);
+    }
+
+    public static void logoutUser() {
+
+        VaadinSession vaadinSession = UI.getCurrent().getSession();
+        vaadinSession.setAttribute(Roles.Endkunde,null);
+        vaadinSession.setAttribute(Roles.Vertriebler,null);
+        UI.getCurrent().getNavigator().navigateTo(Views.LOGINVIEW);
+    }
+
+
+}
---
+++ src/main/java/org/bonn/se/control/exception/NoSuchUserOrPassword.java
@@ -0,0 +1,7 @@
+package org.bonn.se.control.exception;
+
+public class NoSuchUserOrPassword  extends  Exception{
+    public NoSuchUserOrPassword() {
+        super("Fehler Passwort oder Email ist falsch!");
+    }
+}
