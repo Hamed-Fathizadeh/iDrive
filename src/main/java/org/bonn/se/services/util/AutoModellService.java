52eab9130be81adf79bcea77a34aa3a88e90cbb8
Hamed Fathizadeh <hamed.fathizadeh@smail.inf.h-brs.de>
2020-09-08 14:10:25 +0200

    Selenium Test

--- src/main/java/org/bonn/se/services/util/AutoModellService.java
+++ src/main/java/org/bonn/se/services/util/AutoModellService.java
@@ -1,6 +1,4 @@
 
-
-
 package org.bonn.se.services.util;
 
 
---
+++ src/test/java/Selenium/SeleniumTestLogin.java
@@ -0,0 +1,46 @@
+package Selenium;
+
+import org.bonn.se.model.objects.entitites.User;
+import org.bonn.se.services.db.exception.DatabaseException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumTestLogin {
+
+    private static WebDriver driver = null;
+    private static String email = "seleniumtest@carlookemail.de";
+    private User user = new User();
+    @BeforeClass
+    public static void setUpClass(){
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\Hamed\\Desktop\\geckodriver\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void TestLogin() throws InterruptedException, DatabaseException {
+        driver.get("http://localhost:8080/#!LoginView");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div[2]/div")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[2]/div/div[3]/input")).sendKeys("Kornelia.Leiter@idrive.de");
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[2]/div/div[5]/input")).sendKeys("12345678");
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div[2]/div/div[2]/div/div[9]/div")).click();
+        waitMethode();
+        driver.close();
+    }
+
+   public synchronized void waitMethode() throws InterruptedException {
+       driver.wait(2000);
+   }
+
+
+
+}
+
+
+
+
